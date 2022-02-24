46cc65510817ba99bc64c9861c3ea411adbdbd67
aduranm78 <89169122+aduranm78@users.noreply.github.com>
2022-02-24 09:49:25 -0600

    Ajuste tiempo

--- src/main/java/com/example/MySpringBootRouter.java
+++ src/main/java/com/example/MySpringBootRouter.java
@@ -35,8 +35,8 @@ public class MySpringBootRouter extends RouteBuilder {
     		});
     		// .continued(true); // Para continuar con la ruta
 
-		//from("timer:poll?period={{timer.period}}").routeId("{{route.id}}")
-    	from("timer:poll?period={{timer.period}}&delay={{timer.delay}}").routeId("{{route.id}}")
+		from("timer:poll?period={{timer.period}}").routeId("{{route.id}}")
+    	//from("timer:poll?period={{timer.period}}&delay={{timer.delay}}").routeId("{{route.id}}")
     		.process(exchange -> {
     			String wmsUri = env.getProperty("wms.uri");
 				System.out.println("URL WMS: " + wmsUri);
